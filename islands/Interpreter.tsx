9ca069de629e72b22fc2e56cf0a13c76ad617536
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2025-02-17 16:57:23 -0800

    drop execute

--- islands/Interpreter.tsx
+++ islands/Interpreter.tsx
@@ -1,9 +1,9 @@
 import { useState } from "preact/hooks";
-import { execute } from "@swanfactory/hclang";
+// import { execute } from "@swanfactory/hclang";
 
 function evaluateCode(code: string): string {
   console.log(`Evaluating code: ${code}`);
-  console.log(execute);
+  // console.log(execute);
   try {
     const result = code.toUpperCase();
     console.log(`Result: ${result}`);
